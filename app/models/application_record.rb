8aa1b9a2d3fd8dc9a4f1f47378f5ef8cfaa5048d
EC2 Default User <ec2-user@ip-172-31-43-240.ap-northeast-1.compute.internal>
2024-06-18 13:37:59 +0000

    [Finish9]

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
+
+
+  has_many :post_images, dependent: :destroy
 end
